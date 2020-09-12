bb442d1e0528ee566749b13412bfc49f281d0a01
akniet18 <akniet.akniet2016@mail.ru>
2020-09-12 15:31:13 +0600

    first

--- products/cron.py
+++ products/cron.py
@@ -1,15 +1,15 @@
 from django_cron import CronJobBase, Schedule
 from .views import send_push
 
-class MyCronJob(CronJobBase):
-    RUN_EVERY_MINS = 1 # every 2 hours
-    RETRY_AFTER_FAILURE_MINS = 1
+# class MyCronJob(CronJobBase):
+#     RUN_EVERY_MINS = 1 # every 2 hours
+#     RETRY_AFTER_FAILURE_MINS = 1
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
-    code = 'products.send_push'    # a unique code
+#     schedule = Schedule(run_every_mins=RUN_EVERY_MINS, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
+#     code = 'products.send_push'    # a unique code
 
-    def do(self):
-        send_push()
+#     def do(self):
+#         send_push()
 
 
 def MyCronJob():
