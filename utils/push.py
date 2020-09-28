1d322e4410575caabd0a458b338fc05cc178b6df
akniet18 <akniet.akniet2016@mail.ru>
2020-09-29 01:54:11 +0600

    first

--- utils/push.py
+++ utils/push.py
@@ -5,7 +5,6 @@ from push_notifications.models import APNSDevice, GCMDevice
 def send_push(user, text, action=None):
     a = APNSDevice.objects.filter(user = user)
     b = GCMDevice.objects.filter(user = user)
-    print(a)
     if a.exists():
         a[0].send_message(text, badge=1, sound="default")
     elif b.exists():
