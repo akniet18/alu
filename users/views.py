0c1e76bf7ef98b7fe7ff7a83f7a69f5b76893146
akniet18 <akniet.akniet2016@mail.ru>
2020-10-02 20:46:59 +0600

    first

--- users/views.py
+++ users/views.py
@@ -52,7 +52,7 @@ class PhoneCode(APIView):
                     PhoneOTP.objects.create(phone=phone, otp=str(rand), nickname=nickname)
                     # PhoneOTP.objects.create(phone=phone, nickname=nickname, otp=str(1111))
             if phone != "+77783579279":
-                smsc.send_sms(phone, "Ваш код ALU: "+str(rand), sender="sms")
+                smsc.send_sms(phone, "Код подтверждения для ALU.KZ: "+str(rand), sender="sms")
             return Response({'status': 'ok'})
         else:
             return Response(s.errors)
@@ -119,7 +119,7 @@ class LoginUser(APIView):
                     else:
                         PhoneOTP.objects.create(phone=phone, otp=str(rand))
                 if phone != "+77783579279":
-                    smsc.send_sms(phone, "Ваш код ALU: "+str(rand), sender="sms")
+                    smsc.send_sms(phone, "Код подтверждения для ALU.KZ: "+str(rand), sender="sms")
                 return Response({'status': "ok"})
             else:
                 return Response({'status': 'not found'})
